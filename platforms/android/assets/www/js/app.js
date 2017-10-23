4c4024d96929082e9ba825629e58f914c303cd9d
Mindas <mindaugas@mintis.net>
2017-10-23 22:46:50 +0100

    button size

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -1,35 +1,46 @@
 var myApp = angular.module('myModule', []);
 
-myApp.controller('myFirstController', function($scope) {
-
+myApp.controller('mainController', function($scope) {
+	//defining array for list items
 	var todoItems = [];
-	
+
+	//defining todos.name and todos.description (description not used in app yet). 
 	$scope.todos = {
     name: '',
     description: ''
 	}
 
 	$scope.todoItems = todoItems;
-	// should change name for todos
-
+	
+	//todos factory 
 	$scope.makeNewTodoItem = function () {
-		
+		//checking if input has some value
 		if ($scope.todos.name != '') {
 		
-		$scope.todoItems.push(
-			{
-				name: $scope.todos.name,
-				description: $scope.todos.description
-			}
-		);
-		//clearing input after pushing to array
+		//pushing to array 
+		$scope.todoItems.push({
+			name: $scope.todos.name,
+			description: $scope.todos.description
+		});
+		//clearing input field after pushing to array
 		$scope.todos.name = '';
-	}
+		$scope.todos.description = '';
+	
+		localStorage['todosList'] = JSON.stringify($scope.todoItems);
+		
+		}
 	};
 
+	//adding to localstorage
+	var todoItemsData = localStorage['todosList'];
+		if (todoItemsData) {
+			$scope.todoItems = JSON.parse(todoItemsData);
+		}
+
+		//removing todoItems from array and updating localStorage
 	$scope.deleteTodoItem = function ($index) {
 
 		$scope.todoItems.splice($index, 1);
-
+		localStorage['todosList'] = JSON.stringify($scope.todoItems);
 	};
 });
