03213f56334bda15a9130f2671d5f424a78a079f
Mindas <mindaugas@mintis.net>
2017-10-23 19:26:31 +0100

    cleanup

--- www/js/app.js
+++ www/js/app.js
@@ -1,28 +1,29 @@
 var myApp = angular.module('myModule', []);
 
-myApp.controller('myFirstController', function($scope) {
-
+myApp.controller('mainController', function($scope) {
+	//defining array for list items
 	var todoItems = [];
-	
+
+	//defining todos.name and todos.description (description not used in app yet). 
 	$scope.todos = {
     name: '',
     description: ''
 	}
 
 	$scope.todoItems = todoItems;
-	// should change name for todos
-
+	
 	$scope.makeNewTodoItem = function () {
-		
+		//checking if input has some value
 		if ($scope.todos.name != '') {
 		
+		//pushing to array 
 		$scope.todoItems.push(
 			{
 				name: $scope.todos.name,
 				description: $scope.todos.description
 			}
 		);
-		//clearing input after pushing to array
+		//clearing input field after pushing to array
 		$scope.todos.name = '';
 		$scope.todos.description = '';
 	}
